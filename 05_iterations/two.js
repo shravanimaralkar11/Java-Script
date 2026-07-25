fe37a1592d572c8d4e6ae6739779495779c0d80c
shravanimaralkar11 <shravanimaralkar11@gmail.com>
2026-07-25 18:40:00 +0000

    while, do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// WHILE
+
+// let i = 0
+
+// while(i <= 10){
+//     console.log(i);
+//     i++
+// }
+
+let myArr = ["Shra", "Riya", "Joya", "siya"]
+
+// i = 0;
+// while(i < myArr.length){
+//     console.log(myArr[i]);
+//     i++    
+// }
+
+// DO-WHILE
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+        score++
+} while (score <= 10);
